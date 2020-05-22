45f74f1587fc8b43622f8e73e94806505c9c72e2
Firdavs <Firdavs@DESKTOP-8A0QJ34>
2020-05-22 22:01:52 +0500

    Add project files.

---
+++ HW25_ASP.NET_MVC_CORE/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HW25_ASP.NET_MVC_CORE.Models;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+namespace HW25_ASP.NET_MVC_CORE.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            var conString = "Data Source=localhost;Initial Catalog=AlifAcademy;Integrated Security=True";
+            using (IDbConnection db = new SqlConnection(conString))
+            {
+
+                var personList = db.Query<Person>($"SELECT * FROM PERSON").ToList();
+                return View(personList);
+            }
+
+        }
+
+    }
+}
---
+++ HW25_ASP.NET_MVC_CORE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW25_ASP.NET_MVC_CORE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
